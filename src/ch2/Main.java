ef2695d8edb085ace9c27c843383c74add8d19ac
Alexandr <alex.jeli@mail.ru>
2013-08-28 22:03:03 +0300

    Added AOP and ch4

--- src/ch2/Main.java
+++ src/ch2/Main.java
@@ -5,7 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import ch2.performer.Performer;
 
-
 public class Main {
 
 	/**
@@ -14,18 +13,21 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"ch2/beans.xml");
-		
+
 		Performer duke = (Performer) context.getBean("duke");
 		duke.perform();
-		
+
 		Performer performer = (Performer) context.getBean("poeticDuke");
 		performer.perform();
-		
+
 		Performer kenny = (Performer) context.getBean("kenny");
 		kenny.perform();
-		
+
 		Performer sonny = (Performer) context.getBean("sony");
 		sonny.perform();
+
+		Performer spel = (Performer) context.getBean("spel");
+		spel.perform();
 	}
 
 }
--- src/ch2/SpEL.java
+++ src/ch2/SpEL.java
@@ -1,18 +1,39 @@
 package ch2;
 
+import ch2.instrument.Instrument;
 import ch2.performer.Performer;
 
 /**
  * Class to demonstrate the SpEL
+ * 
  * @author Alexandr
- *
+ * 
  */
-public class SpEL implements Performer{
+public class SpEL implements Performer {
+	private int count;
+	private Instrument instrument;
+	private double pi;
 
 	@Override
 	public void perform() {
-		
+		System.out.println("SpEL expresion:");
+		System.out.println("Count=" + count);
+		instrument.play();
+		System.out.println("pi="+pi);
 	}
-	
 
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
+
+	public void setPi(double pi) {
+		this.pi = pi;
+	}
+
+	
+	
 }
