960af4a6c5c4b0994cf29ea37827775aa4a62abb
Alexandr <alex.jeli@mail.ru>
2013-10-07 21:03:36 +0300

    Finished the example with JDBC Template

---
+++ src/ch5/Main.java
@@ -0,0 +1,32 @@
+package ch5;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"ch5/beans.xml");
+		SpitterDao spitterDao = (SpitterDao)context.getBean("spitterDao");
+		spitterDao.addSpitterOldWay(createSpitter("Alexandr"));
+		spitterDao.addSpitter(createSpitter("Ada"));
+		
+		Spitter spitterFromSelect  = spitterDao.getSpitterById(1);
+		System.out.println(spitterFromSelect);
+
+	}
+
+	private static Spitter createSpitter(String name) {
+		Spitter spitter = new Spitter();
+		spitter.setFullName(name);
+		spitter.setUserName(name);
+		spitter.setPassword("pass");
+		spitter.setEmail(name + "@mail.ru");
+		return spitter;
+	}
+
+}
---
+++ src/ch5/SpitterDao.java
@@ -0,0 +1,12 @@
+package ch5;
+
+
+public interface SpitterDao {
+
+	void addSpitterOldWay(Spitter spitter);
+
+	Spitter getSpitterById(int i);
+
+	void addSpitter(Spitter spitter);
+	
+}
