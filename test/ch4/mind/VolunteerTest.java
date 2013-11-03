42ec786648fd5e945d128e2794b671308b9c9c32
Alexandr <alex.jeli@mail.ru>
2013-11-03 23:18:18 +0200

    Added the home controller

--- test/ch4/mind/VolunteerTest.java
+++ test/ch4/mind/VolunteerTest.java
@@ -1,29 +1,29 @@
 package ch4.mind;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import static org.junit.Assert.assertEquals;
+
 public class VolunteerTest {
 
-	private Thinker volunteer;
-	private MindReader magician;
-	private ApplicationContext context = new ClassPathXmlApplicationContext("ch4/beans.xml");
+    private Thinker volunteer;
+    private MindReader magician;
+    private ApplicationContext context = new ClassPathXmlApplicationContext("ch4/beans.xml");
 
-	@Before
-	public void setUp() {
-	
-		volunteer = (Thinker) context.getBean("volunter");
-		magician = (MindReader) context.getBean("magician");
+    @Before
+    public void setUp() {
 
-	}
+        volunteer = (Thinker) context.getBean("volunter");
+        magician = (MindReader) context.getBean("magician");
 
-	@Test
-	public void magicianShouldReadVolunteersMind() {
-		volunteer.thinkOfSomething("QueenofHearts");
-		assertEquals("QueenofHearts", magician.getThoughts());
-	}
+    }
+
+    @Test
+    public void magicianShouldReadVolunteersMind() {
+        volunteer.thinkOfSomething("QueenofHearts");
+        assertEquals("QueenofHearts", magician.getThoughts());
+    }
 }
